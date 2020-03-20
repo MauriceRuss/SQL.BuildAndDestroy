faca46a95d62439c77a2346eca81c87373a0706b
Mo Russ <mauriceruss302@gmail.com>
2020-03-20 14:44:11 -0400

    ex3

---
+++ answers/exercise3.sql
@@ -0,0 +1,10 @@
+
+USE myNewDB;
+CREATE TABLE Users(
+    UserID Int,
+    LastNAME VARCHAR(255),
+    FirstName VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255)
+    );
+SHOW TABLES;
